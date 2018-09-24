16ae10f9d2c381aa93e3f4f853cba8d31b6e2683
Nataša Andžić <natasa.andzic@yahoo.com>
2018-09-24 22:04:03 +0200

    SplashScreen added

---
+++ app/src/main/java/com/natasaandzic/vegansrbija/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.natasaandzic.vegansrbija;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //setContentView(R.layout.splashxml)
+        //Notice that we do not have setContentView() for this SplashActivity.
+        // View is displaying from the theme and this way it is faster than creating a layout.
+
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
