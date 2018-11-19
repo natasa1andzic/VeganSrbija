96be72ee7fb03c90ab2df2ad962b9fc1ab9dd2b0
Natasa <natasa.andzic@yahoo.com>
2018-11-19 14:19:40 +0100

    Colored icons added for fragments

--- app/src/main/java/com/natasaandzic/vegansrbija/activities/BelgradeActivity.java
+++ app/src/main/java/com/natasaandzic/vegansrbija/activities/BelgradeActivity.java
@@ -13,10 +13,12 @@ import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.natasaandzic.vegansrbija.R;
 import com.natasaandzic.vegansrbija.adapters.MyFragmentPagerAdapter;
 import com.natasaandzic.vegansrbija.fragments.MapsFragment;
+import com.natasaandzic.vegansrbija.fragments.ProfileFragment;
 import com.natasaandzic.vegansrbija.fragments.RestaurantsFragment;
 import com.natasaandzic.vegansrbija.fragments.ShopsFragment;
 
@@ -25,6 +27,7 @@ public class BelgradeActivity extends AppCompatActivity{
 	final Fragment fragment1 = new RestaurantsFragment();
 	final Fragment fragment2 = new ShopsFragment();
 	final Fragment fragment3 = new MapsFragment();
+	final Fragment fragment4 = new ProfileFragment();
 	final FragmentManager fm = getSupportFragmentManager();
 	Fragment active = fragment1;
 
@@ -35,6 +38,7 @@ public class BelgradeActivity extends AppCompatActivity{
 
 		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 		navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+		navigation.setItemIconTintList(null);
 
 		fm.beginTransaction().add(R.id.main_container, fragment3, "3").hide(fragment3).commit();
 		fm.beginTransaction().add(R.id.main_container, fragment2, "2").hide(fragment2).commit();
